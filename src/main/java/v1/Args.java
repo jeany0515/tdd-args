7b36aeded1ea6253a753e859ba1130d96328b328
devxu <devxu@thoughtworks.com>
2020-09-06 11:31:12 +0800

    [args-v1][jeany]: extract split args method

--- src/main/java/v1/Args.java
+++ src/main/java/v1/Args.java
@@ -5,15 +5,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Args {
-    private final String command;
     private final List<Arg> args = new ArrayList<>();
 
     public Args(String command, Schema schema) {
-        this.command = command.trim();
-        String[] list = " ".concat(this.command).replaceAll("\\s+", " ").split(" -");
+        List<String> args = splitToArgs(command.trim());
+        args.forEach(arg -> this.args.add(new Arg(arg, schema)));
+    }
+
+    private List<String> splitToArgs(String command) {
+        String[] list = " ".concat(command).replaceAll("\\s+", " ").split(" -");
         List<String> args = new ArrayList<>(Arrays.asList(list));
         args.remove(0);
-        args.forEach(arg -> this.args.add(new Arg(arg, schema)));
+        return args;
     }
 
     public Object getValueOf(String flag) {
